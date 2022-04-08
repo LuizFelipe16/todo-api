71e0f6ff2278741b24b4c62439b00e1076183d10
Luiz Felipe <felipefelizatti215@gmail.com>
2022-04-08 01:24:34 -0300

    API documentation

--- src/server.js
+++ src/server.js
@@ -1,3 +1,7 @@
+const swaggerUi = require('swagger-ui-express');
+const swaggerFile = require('./swagger.json');
 const app = require('./');
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.listen(3333, () => console.log('Server API is Running! --PORT 3333'));
